eb06b43f0d476fd6d9a65c0b0b7d069f9ef59982
txx <979270996@qq.com>
2017-04-11 10:46:42 +0800

    first commit

---
+++ js/globalFunction.js
@@ -0,0 +1,9 @@
+var action={
+	handlerClick:function(){
+		alert(1)
+	},
+	handlerTouch:function(event){
+		console.log(event);
+		alert(2)
+	}
+}
---
+++ js/styleSheets.js
@@ -0,0 +1,6 @@
+var styleSheets={
+		h2:{color:'red'},
+		div:{background:'cyan'},
+		p:{color:'yellow',fontSize:'15px'}
+
+	}
